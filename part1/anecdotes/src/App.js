21e81d50e6873f623cd63b4038c4915502d45d64
Aryan <aryansharma1323@gmail.com>
2021-12-30 03:23:26 +0530

    Updating Anecdotes, Need to fix error
    Anecdotes voting part has an error. Check app.js for details

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,64 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// Header component, Renders Course Name
-const Header = (props) => {
+const Display = (props) => {
 	return (
-		<h1>{props.course}</h1>
+		<div>
+			<div>{props.anecdote}</div>
+			<div>has {props.votes} votes</div>
+		</div>
 	)
 }
 
-// Used in component Content
-// Renders Course part and number of exercises
-const Part = (props) => {
-	return (
-		<>
-			<p>
-				{props.part} {props.exercises}
-			</p>
-		</>
-	)
-}
-
-// Uses Part component to display part and exercises 
-const Content = (props) => {
-	return (
-		<>
-			<Part part = {props.part1} exercises = {props.exercises1} />
-			<Part part = {props.part2} exercises = {props.exercises2} />
-			<Part part = {props.part3} exercises = {props.exercises3} />
-		</>
-	)
-}
-
-// Renders Total exercises in the course
-const Total = (props) => {
-	return (
-		<>
-			<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-		</>
-	)
-}
+const Button = (props) => (
+	<button onClick={props.handleClick}>{props.text}</button>
+)
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(new Array(anecdotes.length).fill(0))
+	const [maximum, setMaximum] = useState("")
+
+	const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+	const handleVote = () => {
+		/*
+			* Issue here: Even when I change state, the changes are not happening until end of function 
+		*/
+
+		// Initialize maximum to the current anecdote
+		if(maximum === ""){
+			setMaximum(selected)
+		}
+		
+		let copyVote = [...vote]
+		copyVote[selected]++
+		setVote(copyVote)
+
+		// Change maximum
+		// When it's >, this is not working properly
+		if(vote[selected] >= vote[maximum]){
+			setMaximum(selected)
+		}
+	}
 
   return (
     <div>
-		<Header course={course} />
-		
-		<Content part1={part1} part2={part2} part3 = {part3} 
-				 exercises1={exercises1} exercises2 = {exercises2} 
-				 exercises3 = {exercises3} />
-
-		<Total exercises1={exercises1} exercises2 = {exercises2} 
-			   exercises3 = {exercises3} />
-    </div>
+				<Display anecdote={anecdotes[selected]} votes={vote[selected]}/>
+				<Button handleClick={handleVote} text="vote"/>
+				<Button handleClick={handleClick} text="next anecdote"/>
+				<h1>Anecdote with most votes</h1>
+				<Display anecdote={anecdotes[maximum]} votes={vote[maximum]}/>
+	</div>
   )
 }
 
